652416f9cd00a74927e08e45d624ba5ccdedfaf4
hustic <sotgouli@gmail.com>
2021-09-26 14:20:58 +0100

    Added f_teams for all the facts of the TI teams, made dim_teams through sql instead

--- fantasy/python/load_get.py
+++ fantasy/python/load_get.py
@@ -9,8 +9,7 @@ class LoadData(PythonTask):
 
         base_url = self.parameters['base_url']
         api_key = self.parameters['api_key']
-        teams = self.parameters['teams']
-        filter = bool(self.parameters['filter'])
+
         keys_to_drop = self.parameters['keys_drop']
         api_call = self.parameters['api_call']
         name = self.parameters['name']
@@ -28,21 +27,14 @@ class LoadData(PythonTask):
             resp = requests.get(
                     f'{base_url}/{api_call}?api_key={api_key}').json()
 
-        with self.step("Filter"):    
-            if filter:
-                filtered = [r for r in resp if r['team_id'] in teams]
-            else:
-                filtered = resp
-
         with self.step('Drop Columns'):
             data = []
-            for r in filtered:
+            for r in resp:
                 for key in keys_to_drop:
                     r.pop(key, None)
                 data.append(r)
-            filtered = data
         
         with self.step(f'Load {name}'):
-            self.default_db.load_data(f"logs_{name}", filtered, replace=True)
+            self.default_db.load_data(f"logs_{name}", data, replace=True)
 
         return self.success()
