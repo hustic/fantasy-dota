e349ae17a6de346d2719035f4e22645fa991e11f
hustic <sotgouli@gmail.com>
2021-09-26 15:06:19 +0100

    Added multi get functionality

--- fantasy/sql/dim_players.sql
+++ fantasy/sql/dim_players.sql
@@ -2,5 +2,5 @@ SELECT p.account_id
      , p.name
      , p.fantasy_role
      , p.team_id
-  FROM logs_players AS p, logs_teams AS t
+  FROM logs_players AS p, dim_teams AS t
  WHERE p.team_id = t.team_id
