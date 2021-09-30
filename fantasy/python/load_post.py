2677c2c37e295a6942a21f8e01b9a6d696a77e07
Sotiris Gkoulimaris <30499756+hustic@users.noreply.github.com>
2021-09-30 19:51:14 +0100

    Finished implementing the data modelling part

--- fantasy/python/load_post.py
+++ fantasy/python/load_post.py
@@ -1,8 +1,12 @@
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 import json
 import time
+
 from pathos.helpers import cpu_count
 from p_tqdm import p_umap
+from tqdm import tqdm
 
 from sayn import PythonTask
 
@@ -20,39 +24,62 @@ class LoadData(PythonTask):
         base_url = self.parameters['base_url']
         api_key = self.parameters['api_key']
 
-        match_ids = self.default_db.read_data(self.query)
-        match_ids = [list(d.values())[0] for d in match_ids]
+        self.set_run_steps(
+            [
+                "Get match_ids from DB",
+                "Submit jobs",
+                "Check for jobs done",
+                "Load Jobs",
+            ]
+        )
 
-        # print(match_ids[:1])
+        with self.step("Get match_ids from DB"):
+            match_ids = self.default_db.read_data(self.query)
+            match_ids = [list(d.values())[0] for d in match_ids]
 
         def get_job_id(m, base_url, api_key):
-            res = {'match_id': m, 'job_id': requests.post(
-                f'{base_url}/request/{m}?api_key={api_key}').json()['job']['jobId'],
-                'active': True}
-            # print(res)
-            time.sleep(float(cpu_count())*0.5)
+            retry_strategy = Retry(
+                total=10,
+                status_forcelist=(429, 500, 502, 503, 504),
+                backoff_factor=2
+            )
+            adapter = HTTPAdapter(max_retries=retry_strategy)
+            http = requests.Session()
+            http.mount("https://", adapter)
+            http.mount("http://", adapter)
+            request = http.post(
+                f'{base_url}/request/{m}?api_key={api_key}')
+
+            try:
+                job_id = request.json()['job']['jobId']
+                res = {'match_id': m, 'job_id': job_id, 'active': True}
+            except:
+                print(request)
+
+            time.sleep(float(cpu_count())*0.05)
             return res
 
-        jobs = []
-        # print(pa.helpers.cpu_count())
+        with self.step("Submit jobs"):
+            jobs = []
 
-        jobs = p_umap(get_job_id, match_ids, [base_url for m in match_ids], [
-                            api_key for m in match_ids])
-        # jobs = [requests.post(f'{base_url}/request/{m}?api_key={api_key}').json()[
-        #                       'job']['jobId'] for m in match_ids[:1]]
-        # jobs = [155900072]
-        # print(jobs)
+            jobs = p_umap(get_job_id, match_ids, [base_url for m in match_ids], [
+                                api_key for m in match_ids])
 
-        job_ids = [j['job_id'] for j in jobs]
+            job_ids = [j['job_id'] for j in jobs]
 
-        while job_ids:
-            for i, job in enumerate(job_ids):
-                resp = requests.get(
-                    f'{base_url}/request/{job}?api_key={api_key}')
-                if resp.text == 'null':
-                    del job_ids[i]
-                time.sleep(0.5)
+        with self.step("Check for jobs done"):
+            pbar = tqdm(total=len(job_ids))
+            while job_ids:
+                for i, job in enumerate(job_ids):
+                    resp = requests.get(
+                        f'{base_url}/request/{job}?api_key={api_key}')
+                    if resp.text == 'null':
+                        del job_ids[i]
+                        pbar.update(1)
+                    time.sleep(0.05)
+            pbar.close()
 
-        self.default_db.load_data("logs_jobs", jobs, replace=True)
+        with self.step("Load Jobs"):
+            self.default_db.load_data("logs_jobs", jobs, replace=True)
 
         return self.success()
--- fantasy/python/load_requests.py
+++ fantasy/python/load_requests.py
@@ -1,4 +1,5 @@
 import requests
+from pathlib import Path
 import json
 import time
 
@@ -23,8 +24,19 @@ class LoadData(PythonTask):
         base_url = self.parameters['base_url']
         api_key = self.parameters['api_key']
 
-        match_ids = self.default_db.read_data(self.query)
-        match_ids = [list(d.values())[0] for d in match_ids]
+        self.set_run_steps(
+            [
+                "Get match_ids from DB",
+                "Get match details",
+                "Format Output & get Timeseries data",
+                "Save Timeseries data",
+                "Load Player Match Stats"
+            ]
+        )
+
+        with self.step("Get match_ids from DB"):
+            match_ids = self.default_db.read_data(self.query)
+            match_ids = [list(d.values())[0] for d in match_ids]
 
         def get_request(m, base_url, api_key, schema):
             response = requests.get(
@@ -42,11 +54,31 @@ class LoadData(PythonTask):
                 players.append(player)
             return players
 
-        data = p_umap(get_request, match_ids, [base_url for m in match_ids], [
+        with self.step("Get match details"):
+            data = p_umap(get_request, match_ids, [base_url for m in match_ids], [
                             api_key for m in match_ids], [self.schema for m in match_ids])
 
-        data = [i for t in data for i in t]
-        self.default_db.load_data(
-            "logs_player_match_stats", data, replace=True, batch_size=500)
+        with self.step("Format Output & get Timeseries data"):
+            final = []
+            timeseries = []
+            for dat in data:
+                for d in dat:
+                    ts = {"account_id": d["account_id"], "match_id": d["match_id"],
+                          "gold_t": d["gold_t"], "xp_t": d["xp_t"], "lh_t": d["lh_t"], "dn_t": d["dn_t"]}
+                    timeseries.append(ts)
+                    del d["gold_t"]
+                    del d["xp_t"]
+                    del d["lh_t"]
+                    del d["dn_t"]
+                    final.append(d)
+
+        with self.step("Save Timeseries data"):
+            file = Path('./timeseries/match_data.json')
+            with open(file, 'w') as f:
+                json.dump(timeseries, f, indent=2)
+
+        with self.step("Load Player Match Stats"):
+            self.default_db.load_data(
+                "logs_player_match_stats", final, replace=True, batch_size=500)
 
         return self.success()
